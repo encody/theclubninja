97eac93ee6df5a8b2b2240fb4085364ee480124c
Jacob <encody@users.noreply.github.com>
2022-05-15 22:02:05 -0500

    backup script

--- backup.sh
+++ backup.sh
@@ -4,12 +4,12 @@ now=$(date +%F-%H-%M-%S)
 mkdir -p backups/$now
 cd backups/$now
 
-fuego query chargeTypes > chargeTypes.json
-fuego query charges > charges.json
-fuego query creditTypes > creditTypes.json
-fuego query invoices > invoices.json
-fuego query memberTypes > memberTypes.json
-fuego query members > members.json
-fuego query memberships > memberships.json
-fuego query terms > terms.json
-fuego query users > users.json
+fuego query --limit 99999 chargeTypes > chargeTypes.json
+fuego query --limit 99999 charges > charges.json
+fuego query --limit 99999 creditTypes > creditTypes.json
+fuego query --limit 99999 invoices > invoices.json
+fuego query --limit 99999 memberTypes > memberTypes.json
+fuego query --limit 99999 members > members.json
+fuego query --limit 99999 memberships > memberships.json
+fuego query --limit 99999 terms > terms.json
+fuego query --limit 99999 users > users.json
